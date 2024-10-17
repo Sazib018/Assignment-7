614a9b4c9a945ac34f03d96f6315f31e215b5ff9
Sazib <mahiburislamsazib@gmail.com>
2024-10-17 15:42:38 +0600

    recipe card desine

--- src/component/RecipeCard/RecipeCard.jsx
+++ src/component/RecipeCard/RecipeCard.jsx
@@ -51,7 +51,7 @@ const RecipeCard = () => {
 
                                 <button
                                     onClick={() => handelToCook(recipe)}
-                                    className="mt-2 px-6 py-3 rounded-[50px] bg-green-500 text-black font-semibold hover:bg-green-600"
+                                    className="mt-2 px-6 py-3 rounded-[50px] bg-green-500 text-black font-semibold hover:bg-red-600"
                                 >
                                     Want to Cook
                                 </button>
@@ -62,10 +62,10 @@ const RecipeCard = () => {
 
             {/* Sidebar */}
             <div className="w-full lg:w-[650px] mt-8 lg:mt-0 lg:ml-6 border p-6 rounded-lg shadow">
-                <h2 className="text-2xl font-bold text-center mb-4">Want to Cook: {wantToCookRecipes.length}</h2>
+                <h2 className="text-2xl font-bold text-center mb-4">Want to Cook:({wantToCookRecipes.length}) </h2>
 
                 <div className="w-full">
-                    <div className="text-left font-medium mt-8 text-[#878787] grid grid-cols-3 gap-2">
+                    <div className="text-left font-medium mt-8 text-[#878787] grid grid-cols-3 gap-2 lg:ml-3">
                         <div>Name</div>
                         <div>Time</div>
                         <div>Calories</div>
@@ -76,14 +76,14 @@ const RecipeCard = () => {
                             wantToCookRecipes.map((recipe) => (
                                 <li key={recipe.recipe_id} className="grid grid-cols-3 gap-4 items-center border-b py-2 mb-7">
                                     <div className="flex items-center">
-                                        <span className="text-[#282828CC] font-semibold mr-2">{recipe.recipe_id}</span> 
+                                        <span className="text-[#282828CC] font-semibold mr-2">{recipe.recipe_id}:</span> 
                                         <span>{recipe.recipe_name}</span>
                                     </div>
                                     <div>{recipe.preparing_time}</div>
                                     <div className="flex justify-between items-center">
                                         <span>{recipe.calories}</span>
                                         <button
-                                            className="md:px-4 md:py-3 bg-[#0BE58A] text-black rounded-[50px]"
+                                            className="md:px-4 md:py-3 bg-[#0BE58A] text-black rounded-[50px] hover:bg-green-800 hover:text-white "
                                         >
                                             Preparing
                                         </button>
